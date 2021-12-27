f0d8b624d69843532c561b56d4534e63759aea48
Rodrigo Ortiz de Zarate <rodrigoodz@gmail.com>
2021-12-27 17:34:38 -0300

    cambiado cardColor

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -3,7 +3,7 @@ const colors = {
   secondary: "#34495e",
   backgroundColor: "#B1D0E0",
   tabNavigatorColor: "#406882",
-  cardColor: "#6998AB",
+  cardColor: "#277da1",
   textPrimary: "#1A374D",
   textSecondary: "#0a9396",
 };
