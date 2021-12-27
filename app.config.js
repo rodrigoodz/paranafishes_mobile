abaded87ddaf7a86d96a0ec76c4733c16c9e6bd7
Rodrigo Ortiz de Zarate <rodrigoodz@gmail.com>
2021-12-27 12:05:23 -0300

    teclado ya no empuja la tabBar

--- app.config.js
+++ app.config.js
@@ -24,6 +24,7 @@ export default {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#FFFFFF",
       },
+      softwareKeyboardLayoutMode: "pan",
     },
     web: {
       favicon: "./assets/favicon.png",
