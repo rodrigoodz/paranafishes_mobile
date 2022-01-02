190b063a4a8414b08538c2991b248c7658fe1c2a
Rodrigo Ortiz de Zarate <rodrigoodz@gmail.com>
2022-01-02 12:49:39 -0300

    cambiado cardColor y primary

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,9 +1,9 @@
 const colors = {
-  primary: "#6998AB",
+  primary: "#ade8f4",
   secondary: "#34495e",
   backgroundColor: "#B1D0E0",
   tabNavigatorColor: "#406882",
-  cardColor: "#277da1",
+  cardColor: "#caf0f8",
   textPrimary: "#1A374D",
   textSecondary: "#0a9396",
 };
